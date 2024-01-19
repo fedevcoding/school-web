82dbd356a978b717c781859c3e89349bc862b5b3
fedevcoding <fedevcoding@gmail.com>
2024-01-19 09:32:48 +0100

    inline to ext css es 8

--- scripts/generateNewWeb.js
+++ scripts/generateNewWeb.js
@@ -3,28 +3,30 @@ const { WEB_PATH } = require("./utils");
 const fs = require("fs").promises;
 
 async function main() {
-  try {
-    const name = process.argv[2];
-    if (!name) {
-      console.log("Please specify a name for the new web");
-      return;
-    }
+ try {
+  const name = process.argv[2];
+  if (!name) {
+   console.log("Please specify a name for the new web");
+   return;
+  }
 
-    const webPath = path.join(WEB_PATH, name);
-    const cssPath = path.join(WEB_PATH, name, "css");
-    const pagesPath = path.join(WEB_PATH, name, "pages");
-    const imagesPath = path.join(WEB_PATH, name, "images");
+  const webPath = path.join(WEB_PATH, name);
+  const cssPath = path.join(WEB_PATH, name, "css");
+  const pagesPath = path.join(WEB_PATH, name, "pages");
+  const imagesPath = path.join(WEB_PATH, name, "images");
 
-    await fs.mkdir(webPath);
-    await fs.writeFile(
-      path.join(webPath, "index.html"),
-      `<!DOCTYPE html>
-<html lang="en">
+  await fs.mkdir(webPath);
+  await fs.writeFile(
+   path.join(webPath, "index.html"),
+   `<!DOCTYPE html>
+<html lang="it">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Title</title>
+    <title>${name}</title>
+
+    <link rel='stylesheet' href='css/style.css' />
 </head>
 
 <body>
@@ -33,13 +35,15 @@ async function main() {
 
 </html>
 `
-    );
+  );
 
-    await fs.mkdir(cssPath);
-    await fs.mkdir(pagesPath);
-    await fs.mkdir(imagesPath);
-  } catch (err) {
-    console.log(err);
-  }
+  await fs.mkdir(cssPath);
+  await fs.writeFile(path.join(webPath, "/css", "style.css"), "");
+
+  await fs.mkdir(pagesPath);
+  await fs.mkdir(imagesPath);
+ } catch (err) {
+  console.log(err);
+ }
 }
 main();
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,6 +1,5 @@
 const path = require("path");
 
 module.exports = {
-  CPP_PATH: path.join(__dirname, "../informatica/cpp/esercizi"),
-  WEB_PATH: path.join(__dirname, "../web"),
+ WEB_PATH: path.join(__dirname, "../web"),
 };
